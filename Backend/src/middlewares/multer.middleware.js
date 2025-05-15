1ef090bf1399607a7737d48b95eae25661a64f85
anish <arvik.rai3377@gmail.com>
2025-05-15 09:27:14 +0545

    fixed import issue

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,4 +1,17 @@
 import multer from "multer";
 
-const storage = multer.memoryStorage();
-export const upload = multer({ storage: storage, fieldSize: 20 });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+export const upload = multer({ storage: storage });
+
+////////////////// Stream upload storage  ////////////////////////////////
+const streamStorage = multer.memoryStorage();
+export const streamUpload = multer({ streamStorage, fileSize: 10 });
